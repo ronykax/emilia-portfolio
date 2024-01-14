009a5cbbc6b60948fb2e22fd00e7a215d815520a
ronykax <ronykax@proton.me>
2024-01-14 13:19:08 +0000

    setup tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let x = "Hello, world!";
+</script>
+
+<h1>{x}</h1>
